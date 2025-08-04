2b80424fd30991ebdd31bc19c086949d77a8ae13
Aakash <aakashkumarpy@gmail.com>
2025-08-04 05:14:23 +0000

    making changes..

--- django-server/core/settings.py
+++ django-server/core/settings.py
@@ -9,13 +9,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-# SECRET_KEY = config('SECRET_KEY')
-SECRET_KEY = 'django-insecure-mytlo!-o7l!(vh86*1yk0wb%=%vsbn9!)#v5g(4#$c)fbr*3o@'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://hms-production-76c7.up.railway.app/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
